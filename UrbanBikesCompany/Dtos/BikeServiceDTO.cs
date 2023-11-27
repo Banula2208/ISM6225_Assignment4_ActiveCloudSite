765d2d6a996d1ba90e19da45e445446ec549300e
Dinesh Datta Pendyala <dineshdatta@usf.edu>
2023-11-26 22:33:36 -0500

    dtos

---
+++ UrbanBikesCompany/Dtos/BikeServiceDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace UrbanBikesCompany.DTos
+{
+    public class BikeServiceDTO
+    {
+        public string serviceId { get; set; }
+
+        public string bikeId { get; set; }
+
+        public float price { get; set; }
+
+
+
+        public string bikename { get; set; }
+
+        public string serviceName { get; set; }
+
+        public int bsId { get; set; }
+    }
+}
