83bd939f9c0b6134c3b54a0bb69f559e9c1c5937
banula007 <137216446+banula007@users.noreply.github.com>
2023-11-26 19:59:21 -0500

    Added Models

---
+++ UrbanBikesCompany/Models/BikeServiceModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace UrbanBikesCompany.Models
+{
+    public class BikeServiceModel
+    {
+        public int bs_id { get; set; }
+
+        public int service_id { get; set; }
+
+
+        public int bike_id { get; set; }
+
+        public float price { get; set;}
+
+        public ServiceModel service { get; set; }
+
+
+        public MototBikes bike { get; set; }
+
+        public virtual ServiceOrder? ServiceOrder { get; set; }
+
+        public int? OrderId { get; set; }
+
+    }
+
+    public class BikeServiceModelConfiguration : IEntityTypeConfiguration<BikeServiceModel>
+    {
+        public void Configure(EntityTypeBuilder<BikeServiceModel> builder)
+        {
+            builder.HasKey(x => new { x.bs_id });
+
+            builder.HasOne(x => x.service).WithMany(x => x.BikeServiceModels).HasForeignKey(x => x.service_id).OnDelete(DeleteBehavior.ClientSetNull);
+
+            builder.HasOne(x => x.bike).WithMany(x => x.BikeServiceModels).HasForeignKey(x => x.bike_id).OnDelete(DeleteBehavior.ClientSetNull);
+            builder.HasOne(x => x.ServiceOrder).WithMany(x => x.BikeServiceModel).HasForeignKey(x => x.OrderId).OnDelete(DeleteBehavior.ClientSetNull);
+
+
+
+        }
+
+    }
+}
---
+++ UrbanBikesCompany/Models/MototBikes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace UrbanBikesCompany.Models
+{
+    public class MototBikes
+    {
+        public int bikeId { get; set; }
+
+        public string bikeName { get; set; }
+
+        public string type { get; set; }
+
+        public virtual List<BikeServiceModel>? BikeServiceModels { get; set; }
+    }
+
+    public class MototBikesModelConfiguration : IEntityTypeConfiguration<MototBikes>
+    {
+        public void Configure(EntityTypeBuilder<MototBikes> builder)
+        {
+            builder.HasKey(x => new { x.bikeId });
+        }
+
+    }
+}
